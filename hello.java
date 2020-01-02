c1c60d6cf8b0cb6079cd2e0c725910d1254958ba
wuxb23 <18720940930@163.com>
2020-01-02 16:10:51 +0800

    添加启动文件

--- hello.java
+++ hello.java
@@ -1,4 +1,5 @@
 public class Hello{
+	//2020.1.2好好加油
 	public static void sayHello(String msg){
 		System.out.println(msg);
 		}
---
+++ startup.java
@@ -0,0 +1,7 @@
+public class startup{
+	public static void main(String ...args){
+		Hello.sayHello("2020年，万事顺意，达到人生巅峰，哈哈。。。");
+	}
+	
+	
+}
